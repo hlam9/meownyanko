26ae4559469cdc0140902f7339daba6d60aa0d22
Jay Lee <o3onoobmule@gmail.com>
2023-12-18 18:00:03 -0800

    oopsie

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -9,6 +9,7 @@ function PageNotFound(){
             Page not Found</h1>
             <br />
             <img src="https://cdn.7tv.app/emote/61f18e81170449495610fc7a/3x.webp" alt="catSad" />
+            <img src="https://cdn.7tv.app/emote/64d6812e6c817df67d590db5/3x.webp" alt="oopsie" />
             
 		</div>
     );
